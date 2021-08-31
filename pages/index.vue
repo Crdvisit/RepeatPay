04d77b142e49c280220cf13ff7bd66f86e18b79d
Mert Doğu <mertdoguonder@gmail.com>
2021-08-31 16:44:50 +0300

    changed: text align, footer, and deleted img

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="flex flex-col justify-center items-center">
     <div class="md:flex grid main justify-items-center space-x-5 md:justify-center items-center">
-      <div class="grid justify-items-center main text-center md:text-left md:block">
+      <div class="grid justify-items-center main text-center md:text-center md:block">
         <h1 class="font-extrabold text-5xl">RepeatPay</h1>
         <p class="mt-4">
           New open-source donate system for community. Easy, fast and secure.
         </p>
       </div>
-      <img src="/main.jpg" class="flex mt-4 img md:mt-0 w-3/6 h-auto" alt="">
     </div>
   </div>
 </template>
