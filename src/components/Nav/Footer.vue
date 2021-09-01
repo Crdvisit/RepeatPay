186e7893b50bdf7203bad5e50cc7a9285e8ad824
Mert Doğu <mertdoguonder@gmail.com>
2021-09-01 18:01:04 +0300

    style: hover to footer

--- src/components/Nav/Footer.vue
+++ src/components/Nav/Footer.vue
@@ -3,7 +3,10 @@
     class="h-auto p-4 bg-gray-200 text-gray-900 dark:(bg-gray-900 text-gray-50) transition ease-out duration-700 "
   >
     <div class="flex justify-between items-center">
-      <h1 class="font-normal text-lg">
+      <h1 class="font-normal
+      text-lg
+      ml-1
+      transform motion-safe:hover:(scale-110) transition ease duration-400 ">
         RepeatPay Team
       </h1>
       <div class="space-x-5">
